17a54846d39ad0c5b1921abf80e498e3c769dc91
Ricardo Gutiérrez <25915250+ricguti@users.noreply.github.com>
2020-06-27 19:12:17 -0500

    Add markers to MapView

--- lib/map-view.dart
+++ lib/map-view.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutterhackatonapp/components/custom_search_bar.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -10,7 +12,35 @@ class MapView extends StatefulWidget {
 class _MapViewState extends State<MapView> {
   GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  final LatLng _center = const LatLng(19.4007, -99.1573);
+  final Map<int, Marker> _markers = generateMarkers();
+
+  static Map<int, Marker> generateMarkers() {
+    final Map<int, Marker> generatedMarkers = {};
+    for(int i = 0; i < 15; i++) {
+      generatedMarkers[i] = generateMarker(i);
+    }
+    return generatedMarkers;
+  }
+
+  static Marker generateMarker(int markerId) {
+    Random random = new Random();
+    double maxDistance = 0.02;
+    double latitude = 19.4007;
+    double longitude = -99.1573;
+    double plusOrMinus = random.nextDouble() < 0.5 ? 1 : -1;
+    double lat = plusOrMinus * (maxDistance * random.nextDouble()) + latitude;
+    double lng = plusOrMinus * (maxDistance * random.nextDouble()) + longitude;
+
+    return Marker(
+      markerId: MarkerId(markerId.toString()),
+      position: LatLng(lat, lng),
+      infoWindow: InfoWindow(
+        title: "Friendly neigbour",
+        snippet: "Get your stuff here",
+      ),
+    );
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -25,8 +55,9 @@ class _MapViewState extends State<MapView> {
             onMapCreated: _onMapCreated,
             initialCameraPosition: CameraPosition(
               target: _center,
-              zoom: 11.0,
+              zoom: 14.0,
             ),
+            markers: _markers.values.toSet(),
           ),
           SafeArea(
             child: Hero(tag: "SearchBar", child: CustomSearchBar()),
